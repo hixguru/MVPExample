b60bafcab6c506adb9d59ab0764b53c4a1c381a9
hixguru <ikxguru@gmail.com>
2017-03-29 17:32:12 +0900

    labmda 적용

--- app/src/main/java/hixguru/kr/mvpexample/presenter/MainPresenterImpl.java
+++ app/src/main/java/hixguru/kr/mvpexample/presenter/MainPresenterImpl.java
@@ -1,13 +1,8 @@
 package hixguru.kr.mvpexample.presenter;
 
-import android.util.Log;
-import com.google.gson.GsonBuilder;
-import hixguru.kr.mvpexample.model.User;
 import hixguru.kr.mvpexample.network.GitHubApi;
 import hixguru.kr.mvpexample.network.RetrofitCreator;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
@@ -16,8 +11,6 @@ import retrofit2.Retrofit;
  */
 
 public class MainPresenterImpl implements MainContract.Presenter {
-
-    private static final String TAG = MainPresenterImpl.class.getSimpleName();
     private MainContract.View view;
 
     public MainPresenterImpl(MainContract.View view) {
@@ -32,17 +25,9 @@ public class MainPresenterImpl implements MainContract.Presenter {
         api.getUser(userName)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-            .subscribe(new Consumer<User>() {
-                @Override
-                public void accept(@NonNull User user) throws Exception {
-                    Log.i(TAG, new GsonBuilder().setPrettyPrinting().create().toJson(user));
-                    view.showResult(user);
-                }
-            }, new Consumer<Throwable>() {
-                @Override
-                public void accept(@NonNull Throwable throwable) throws Exception {
-                    Log.e(TAG, "accept: error : " + throwable.getMessage());
-                }
-            });
+            .subscribe(
+                user -> view.showResult(user),
+                Throwable::printStackTrace
+            );
     }
 }
--- app/src/main/java/hixguru/kr/mvpexample/view/MainActivity.java
+++ app/src/main/java/hixguru/kr/mvpexample/view/MainActivity.java
@@ -1,26 +1,21 @@
 package hixguru.kr.mvpexample.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.rxbinding2.widget.RxTextView;
-import hixguru.kr.mvpexample.model.User;
 import hixguru.kr.mvpexample.R;
+import hixguru.kr.mvpexample.model.User;
 import hixguru.kr.mvpexample.presenter.MainContract;
 import hixguru.kr.mvpexample.presenter.MainPresenterImpl;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Predicate;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.et_user_name) EditText etUserName;
     @BindView(R.id.tv_result) TextView tvResult;
 
@@ -37,19 +32,11 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         presenter = new MainPresenterImpl(this);
 
         disposable = RxTextView.textChanges(etUserName)
-            .filter(new Predicate<CharSequence>() {
-                @Override
-                public boolean test(@NonNull CharSequence charSequence) throws Exception {
-                    return charSequence.length() > 3;
-                }
-            })
-            .subscribe(new Consumer<CharSequence>() {
-                @Override
-                public void accept(@NonNull CharSequence charSequence) throws Exception {
-                    Log.e(TAG, "accept: charSe" + charSequence);
-                    presenter.loadData(charSequence.toString());
-                }
-            });
+            .filter(charSequence -> charSequence.length() > 3)
+            .subscribe(
+                charSequence -> presenter.loadData(charSequence.toString()),
+                Throwable::printStackTrace
+            );
     }
 
     @Override
